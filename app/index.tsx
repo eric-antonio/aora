350957400d4ce5d20ad95b38c50750d7453fe49a
Éric  António <ericantoniodev@gmail.com>
2024-08-09 23:54:41 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Aora</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
