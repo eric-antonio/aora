218358eceb2519b559559d6f4f6c74d24dbf1b64
Éric  António <ericantoniodev@gmail.com>
2024-08-14 20:54:29 +0200

    Create _layout.tsx

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AuthLayOut = () => {
+  return (
+    <View>
+      <Text>AuthLayOut</Text>
+    </View>
+  )
+}
+
+export default AuthLayOut
+
+const styles = StyleSheet.create({})
