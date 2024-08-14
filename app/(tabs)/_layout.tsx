29036f738e7dbe6f8b81c244c96c990057adfe6a
Éric  António <ericantoniodev@gmail.com>
2024-08-14 20:54:36 +0200

    Create _layout.tsx

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TabsLayOut = () => {
+  return (
+    <View>
+      <Text>TabsLayOut</Text>
+    </View>
+  )
+}
+
+export default TabsLayOut
+
+const styles = StyleSheet.create({})
