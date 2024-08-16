abbf46a298337436d26b94dace8032d5b111b45d
Éric  António <ericantoniodev@gmail.com>
2024-08-16 18:51:04 +0200

    Update _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {  StyleSheet, Text, View } from "react-native";
+import { Stack } from "expo-router";
 
 const AuthLayOut = () => {
   return (
-    <View>
-      <Text>AuthLayOut</Text>
-    </View>
-  )
-}
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+            title: "Sign In",
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+            title: "Sign Up",
+          }}
+        />
+      </Stack>
+      <StatusBar style="light" backgroundColor="#161622" />
+    </>
+  );
+};
 
-export default AuthLayOut
+export default AuthLayOut;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
