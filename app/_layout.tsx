f2a6284292cce20e3af26819638dcc19bb332977
Éric  António <ericantoniodev@gmail.com>
2024-08-16 19:50:26 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,9 +27,14 @@ const RootLayOut = () => {
   if (!fontsLoaded && !error) return undefined;
 
   return (
+    <>
+    <StatusBar style="light" backgroundColor="#161622" />
     <Stack>
+      
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
+    </>
   );
 };
 
