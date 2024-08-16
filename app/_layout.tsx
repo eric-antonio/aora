041fdc4cfa66cc2428a02f69c73ad24764746db0
Éric  António <ericantoniodev@gmail.com>
2024-08-16 20:03:27 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,6 +33,8 @@ const RootLayOut = () => {
       
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
     </>
   );
