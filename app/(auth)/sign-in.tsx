5ebb540a2592b4f47bb5c3e85ea941bfaca9cd1d
Éric  António <ericantoniodev@gmail.com>
2024-08-16 18:58:28 +0200

    Update sign-in.tsx

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[90vh]: px-4 my-6">
+          <Image
+            source={images.logo}
+            className="w-[115px] h-[35px]"
+            resizeMode="contain"
+          />
+          <Text className="text-white  text-semibold font-psemibold text-3xl mt-5">
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default SignIn
+export default SignIn;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
