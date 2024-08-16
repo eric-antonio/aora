3354b7d6a445fccc478e19b8d7975e895b87f7da
Éric  António <ericantoniodev@gmail.com>
2024-08-16 19:01:58 +0200

    Create FormField.tsx

---
+++ components/FormField.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const FormField = () => {
+  return (
+    <View className="space-y-2">
+      <Text className="text-base text-gray-100 font-pmedium">FormField</Text>
+    </View>
+  );
+};
+
+export default FormField;
+
+const styles = StyleSheet.create({});
