03acb40614c701c5be9d9223398c74009efaf4ce
Éric  António <ericantoniodev@gmail.com>
2024-08-21 23:28:54 +0200

    Update home.tsx

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,9 @@
 import { FlatList, Text, View, Image } from "react-native";
 import React from "react";
-import {
-  SafeAreaView,
-} from "react-native-safe-area-context";
-import{images} from '../../constants'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import Search from "../search/[query]";
+import SearchInput from "@/components/SearchInput";
 
 const Home = () => {
   return (
@@ -27,10 +27,21 @@ const Home = () => {
                 </Text>
               </View>
               <View className="mt-1.5">
-                <Image source={images.logoSmall} className="w-9 h-10" resizeMode='contain' />
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
               </View>
             </View>
-            
+            <SearchInput
+              title={""}
+              value={""}
+              placeholder={""}
+              handleChangeText={function (e: string): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
           </View>
         )}
       />
