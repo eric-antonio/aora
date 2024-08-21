ca085863b10a532883a2e484c80704c544365f9a
Éric  António <ericantoniodev@gmail.com>
2024-08-21 23:20:59 +0200

    Update home.tsx

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,9 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, Image } from "react-native";
 import React from "react";
 import {
-  SafeAreaFrameContext,
   SafeAreaView,
 } from "react-native-safe-area-context";
+import{images} from '../../constants'
 
 const Home = () => {
   return (
@@ -11,15 +11,26 @@ const Home = () => {
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <Text className="text-3xl text-white">{item.id}</Text>}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View className="flex-1">
-                <Text className="font-pmedium text-sm text-gray-100">Welcome Back Sr Eric</Text>
-                <Text className="text-2xl font-psemibold text-white"> Eric Antonio</Text>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back Sr Eric
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {" "}
+                  Eric Antonio
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image source={images.logoSmall} className="w-9 h-10" resizeMode='contain' />
               </View>
             </View>
+            
           </View>
         )}
       />
