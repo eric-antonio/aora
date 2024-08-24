fe0c051e65204164f362e0f8a1d8f7c973ed1cae
Éric  António <ericantoniodev@gmail.com>
2024-08-24 20:39:16 +0200

    Create UseAppwrite.ts

---
+++ lib/UseAppwrite.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { Alert } from "react-native";
+
+interface DataProps {
+  id: number;
+  title: string;
+}
+
+const useAppwrite = (fn: () => Promise<any>) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fn();
+      setData(response);
+    } catch (error) {
+      Alert.alert("Bug here!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => fetchData();
+  return { data , isLoading,refetch};
+};
+
+export default useAppwrite;
