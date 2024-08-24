d68c5a310bed40acdbcfa5ef2c5f786c5466810e
Éric  António <ericantoniodev@gmail.com>
2024-08-24 20:52:37 +0200

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -21,7 +21,12 @@ const VideoCard = ({
             />
           </View>
           <View className="justify-center flex-1 ml-3 gap-y-1 ">
-            <Text className="text-white font-psemibold text-sm">{title}</Text>
+            <Text
+              className="text-white font-psemibold text-sm"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
           </View>
         </View>
       </View>
