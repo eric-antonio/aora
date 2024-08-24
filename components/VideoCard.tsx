41ce6d0f57af0a153eb8cc4fd8cb21b40f0fb4bb
Éric  António <ericantoniodev@gmail.com>
2024-08-24 20:45:10 +0200

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const VideoCard = ({
@@ -11,6 +11,17 @@ const VideoCard = ({
 }) => {
   return (
     <View className="flex-col items-center pc-4 mb-14">
+      <View className="flex-row gap-3 items-start">
+        <View className="justify-center items-center flex-row flex-1">
+          <View className="w-[46px] h-[46px] border rounded-lg border-secondary justify-center items-center p-0.5">
+            <Image
+              source={{ uri: avatar }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
+            />
+          </View>
+        </View>
+      </View>
       <Text className="text-2xl text-white">{title}</Text>
     </View>
   );
