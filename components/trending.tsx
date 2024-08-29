27ac0d09eeb6ac2145dc242e4af233eb9daed50d
Éric  António <ericantoniodev@gmail.com>
2024-08-29 10:45:08 +0200

    Update trending.tsx

--- components/trending.tsx
+++ components/trending.tsx
@@ -1,20 +1,99 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import React from "react";
-type TrendingPosts = {
+import { icons } from "@/constants";
+import React, { useState } from "react";
+import {
+  FlatList,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+
+interface TrendingPosts {
   posts: { id: string }[];
+}
+
+interface TrendingItemProps {
+  activeItem: { id: string };
+  item: { id: string };
+}
+
+interface ViewaBleItemsProps {
+  viewableItems: string;
+}
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1.1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }: TrendingItemProps) => {
+  const [paly, setPaly] = useState(false);
+
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {paly ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => setPaly(true)}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[32px] my-5 mx-3 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image
+            className="w-12 h-12 absolute"
+            source={icons.play}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
 };
 
 const Trending = ({ posts }: TrendingPosts) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
+  const viewableItemsChanged = ({ viewableItems }: ViewaBleItemsProps) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
-      renderItem={({ item }) => <Text className="text-white">{item.id}</Text>}
+      renderItem={({ item }) => (
+        <TrendingItem activeItem={activeItem} item={item} />
+      )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{ itemVisiblePercentThreshold: 70 }}
+      contentOffset={{ x: 170 }}
       horizontal
     />
   );
 };
 
 export default Trending;
-
-const styles = StyleSheet.create({});
