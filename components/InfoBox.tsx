dc2a594a958df6eb362770eee0b0dd5eef997487
Éric  António <ericantoniodev@gmail.com>
2024-08-30 21:16:27 +0200

    Create InfoBox.tsx

---
+++ components/InfoBox.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InfoBox = () => {
+  return (
+    <View>
+      <Text>InfoBox</Text>
+    </View>
+  )
+}
+
+export default InfoBox
+
+const styles = StyleSheet.create({})
