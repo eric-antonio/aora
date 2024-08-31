6177e58420ed464d051fbe7a47075b8a2f9c7770
Éric  António <ericantoniodev@gmail.com>
2024-08-31 10:53:41 +0200

    Update InfoBox.tsx

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const InfoBox = () => {
-  return (
-    <View>
-      <Text>InfoBox</Text>
-    </View>
-  )
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+interface InfoBoxProps {
+  title: string;
+  subtitle?: string;
+  containerStyles?: string;
+  titleStyles?: string;
 }
+const InfoBox = ({
+  title,
+  subtitle,
+  containerStyles,
+  titleStyles,
+}: InfoBoxProps) => {
+  return (
+    <View className={containerStyles}>
+      <Text className={`text-white text-center font-psemibold ${titleStyles}`}>
+        {title}
+      </Text>
+      <Text className="text-sm text-gray-100 text-center font-pregular">{subtitle}</Text>
+    </View>
+  );
+};
 
-export default InfoBox
+export default InfoBox;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
